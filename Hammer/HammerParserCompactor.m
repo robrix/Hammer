05adf903b883a492df254ada7c431cbc61de8513
Rob Rix <rob.rix@me.com>
2012-09-30 01:35:26 -0400

    Uses the empty/null predicates to compact parsers to empty/null reduction parsers.

--- Hammer/HammerParserCompactor.m
+++ Hammer/HammerParserCompactor.m
@@ -37,6 +37,18 @@
 }
 
 
+-(HammerParser *)compactParser:(HammerParser *)parser block:(HammerParser *(^)())block {
+	HammerParser *compacted = nil;
+	if (parser.isEmpty)
+		compacted = [HammerEmptyParser parser];
+	else if (parser.isNull)
+		compacted = [[HammerNullReductionParser parserWithParseTrees:parser.parseNull] acceptVisitor:self];
+	else
+		compacted = block();
+	return compacted;
+}
+
+
 -(HammerEmptyParser *)emptyParser:(HammerEmptyParser *)parser {
 	return parser;
 }
@@ -59,26 +71,32 @@
 
 
 -(HammerParser *)alternationParser:(HammerAlternationParser *)parser withLeft:(HammerLazyVisitable)left right:(HammerLazyVisitable)right {
-	if ([self compact:left].isEmpty)
-		return [self compact:right];
-	else if ([self compact:right].isEmpty)
-		return [self compact:left];
-	else
-		return [HammerAlternationParser parserWithLeft:HammerDelay([self compact:left]) right:HammerDelay([self compact:right])];
+	return [self compactParser:parser block:^HammerParser *{
+		if ([self compact:left].isEmpty)
+			return [self compact:right];
+		else if ([self compact:right].isEmpty)
+			return [self compact:left];
+		else
+			return [HammerAlternationParser parserWithLeft:HammerDelay([self compact:left]) right:HammerDelay([self compact:right])];
+	}];
 }
 
 -(HammerParser *)concatenationParser:(HammerConcatenationParser *)parser withFirst:(HammerLazyVisitable)first second:(HammerLazyVisitable)second {
-	if ([self compact:first].isEmpty || [self compact:second].isEmpty)
-		return [HammerEmptyParser parser];
-	else
-		return [HammerConcatenationParser parserWithFirst:HammerDelay([self compact:first]) second:HammerDelay([self compact:second])];
+	return [self compactParser:parser block:^HammerParser *{
+		if ([self compact:first].isEmpty || [self compact:second].isEmpty)
+			return [HammerEmptyParser parser];
+		else
+			return [HammerConcatenationParser parserWithFirst:HammerDelay([self compact:first]) second:HammerDelay([self compact:second])];
+	}];
 }
 
 -(HammerParser *)reductionParser:(HammerReductionParser *)parser withParser:(HammerLazyVisitable)child {
-	if ([self compact:child].isEmpty)
-		return [HammerEmptyParser parser];
-	else
-		return [HammerReductionParser parserWithParser:HammerDelay([self compact:child]) function:parser.function];
+	return [self compactParser:parser block:^HammerParser *{
+		if ([self compact:child].isEmpty)
+			return [HammerEmptyParser parser];
+		else
+			return [HammerReductionParser parserWithParser:HammerDelay([self compact:child]) function:parser.function];
+	}];
 }
 
 @end
