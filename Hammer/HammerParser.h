328f63f0b9719ec6bbab854ccc98d81f9456c3b0
Rob Rix <rob.rix@me.com>
2012-09-30 11:52:31 -0400

    Documents HammerParser just a tiny bit.

--- Hammer/HammerParser.h
+++ Hammer/HammerParser.h
@@ -7,8 +7,13 @@
 
 @interface HammerParser : NSObject <NSCopying>
 
+// parses a full sequence and returns the resulting parse trees
 -(NSSet *)parseFull:(id<NSFastEnumeration>)sequence;
+
+// parses a given term, returning the derivative of self with respect to that term
 -(HammerParser *)parse:(id)term;
+
+// collapses the parser to its corresponding parse forest
 -(NSSet *)parseNull;
 
 // whether or not it can parse the null (empty) string
@@ -21,6 +26,7 @@
 @property (nonatomic, readonly, getter = isEmpty) bool empty;
 
 @end
+
 
 @interface HammerParser () <HammerVisitable> // intended for subclassing
 
