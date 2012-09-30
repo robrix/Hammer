0837107a31ce57b928aafc72c44b0108e0c29b5d
Rob Rix <rob.rix@me.com>
2012-09-30 10:55:25 -0400

    Types the least fixed point visitor’s visitor property correctly.

--- Hammer/HammerLeastFixedPointVisitor.h
+++ Hammer/HammerLeastFixedPointVisitor.h
@@ -9,6 +9,6 @@
 -(instancetype)initWithBottom:(id)bottom visitor:(id<HammerVisitor>)visitor;
 
 @property (readonly) id bottom;
-@property (readonly) id visitor;
+@property (readonly) id<HammerVisitor> visitor;
 
 @end
