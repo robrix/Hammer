41f000f3f280dfa238f3abd5976130d58c2587d1
Rob Rix <rob.rix@github.com>
2014-03-22 03:37:06 -0400

    Kleene’s least fixed point theorem.

---
+++ Hammer/HMRLeastFixedPoint.m
@@ -0,0 +1,21 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import "HMRLeastFixedPoint.h"
+
+id HMRLeastFixedPoint(id least, id(^block)(id)) {
+	bool changed = false;
+	do {
+		id next = block(least);
+		changed = ![least isEqual:next];
+		least = next;
+	} while (changed);
+	return least;
+}
+
+l3_addTestSubjectTypeWithFunction(HMRLeastFixedPoint);
+l3_test(&HMRLeastFixedPoint) {
+	id lfp = HMRLeastFixedPoint(@-0.9, ^(NSNumber *x) {
+		return @(x.doubleValue * x.doubleValue);
+	});
+	l3_expect(lfp).to.equal(@0);
+}
