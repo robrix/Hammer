fa1c347f8029f70addef072154eebec3ccbfcd60
Rob Rix <rob.rix@github.com>
2014-03-25 23:15:16 -0400

    Parse reducibles.

--- Hammer/HMRParserCombinator.h
+++ Hammer/HMRParserCombinator.h
@@ -1,8 +1,9 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 #import <Hammer/HMRCombinator.h>
+#import <Reducers/REDReducible.h>
 
-extern NSSet *HMRParseCollection(id<HMRCombinator> parser, id<NSFastEnumeration> collection);
+extern NSSet *HMRParseCollection(id<HMRCombinator> parser, id<REDReducible> reducible);
 extern id<HMRCombinator> HMRParseElement(id<HMRCombinator> parser, id<NSObject, NSCopying> element);
 
 @interface HMRParserCombinator : NSObject <HMRCombinator>
