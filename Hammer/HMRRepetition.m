9bf7ee4b370762a53b5a56025fe653572ffc656f
Rob Rix <rob.rix@github.com>
2014-03-30 10:39:55 -0400

    Compact repetition in the constructor.

--- Hammer/HMRRepetition.m
+++ Hammer/HMRRepetition.m
@@ -1,10 +1,5 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-#import "HMRAlternation.h"
-#import "HMRConcatenation.h"
-#import "HMREmpty.h"
-#import "HMRLazyCombinator.h"
-#import "HMRReduction.h"
 #import "HMRRepetition.h"
 
 @implementation HMRRepetition
@@ -48,17 +43,6 @@ l3_test(@selector(derivative:)) {
 }
 
 
--(id<HMRCombinator>)compact {
-	return self.parser.compaction == [HMREmpty empty]?
-		HMRCaptureTree(@[])
-	:	[super compact];
-}
-
-l3_test(@selector(compaction)) {
-	l3_expect(HMRRepeat([HMREmpty empty]).compaction).to.equal(HMRCaptureTree(@[]));
-}
-
-
 -(NSString *)describe {
 	return [NSString stringWithFormat:@"%@*", self.parser.description];
 }
@@ -67,5 +51,12 @@ l3_test(@selector(compaction)) {
 
 
 id<HMRCombinator> HMRRepeat(id<HMRCombinator> parser) {
-	return [HMRRepetition combinatorWithParser:parser];
+	parser = parser.compaction;
+	return parser == HMRNone()?
+		HMRCaptureTree(@[])
+	:	[HMRRepetition combinatorWithParser:parser];
+}
+
+l3_test(@selector(compaction)) {
+	l3_expect(HMRRepeat(HMRNone()).compaction).to.equal(HMRCaptureTree(@[]));
 }
