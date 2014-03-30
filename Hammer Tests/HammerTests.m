80ea5f796a3a6f3eed4095de036c738195af0e43
Rob Rix <rob.rix@github.com>
2014-03-30 00:22:33 -0400

    HammerTests.m loads its BUNDLE_LOADER’s test suite

--- Hammer Tests/HammerTests.m
+++ Hammer Tests/HammerTests.m
@@ -1,3 +1,13 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 //  This is a stub to give the test target something to compile. The tests occur inline.
+@interface HammerTests : XCTestCase
+@end
+
+@implementation HammerTests
+
++(id)defaultTestSuite {
+	return [L3TestSuite suiteForExecutablePath:@L3_BUNDLE_LOADER];
+}
+
+@end
