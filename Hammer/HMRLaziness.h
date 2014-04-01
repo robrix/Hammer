7435b2d9a370836363f51ec9d11ee68f683ce26f
Rob Rix <rob.rix@github.com>
2014-03-31 22:08:18 -0400

    Attempt to demonstrate correct collapsing. Fail.

---
+++ Hammer/HMRLaziness.h
@@ -0,0 +1,9 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+@import Foundation;
+
+@protocol HMRLaziness <NSObject>
+
+<##>
+
+@end
--- Hammer/HMRLexer.m
+++ Hammer/HMRLexer.m
@@ -15,7 +15,12 @@
  while computing this, when we determine that we have produced a word, then perform its reduction
  */
 l3_test("null reduction of partially parsed strings") {
-	id<HMRCombinator> alternatives = HMRAlternate(HMRConcatenate(HMRLiteral(@"f"), HMRConcatenate(HMRLiteral(@"o"), HMRConcatenate(HMRLiteral(@"o"), HMRLiteral(@"t")))), HMRConcatenate(HMRLiteral(@"f"), HMRConcatenate(HMRLiteral(@"o"), HMRConcatenate(HMRLiteral(@"o"), HMRLiteral(@"t")))));
+	id<HMRCombinator> alternatives = HMRAlternate(HMRConcatenate(HMRLiteral(@"f"), HMRConcatenate(HMRLiteral(@"o"), HMRConcatenate(HMRLiteral(@"o"), HMRLiteral(@"t")))), HMRConcatenate(HMRLiteral(@"f"), HMRConcatenate(HMRLiteral(@"o"), HMRConcatenate(HMRLiteral(@"o"), HMRLiteral(@"d")))));
 	id<HMRCombinator> derivative = [[[alternatives derivative:@"f"] derivative:@"o"] derivative:@"o"];
+	
+	id<HMRCombinator> derivative2 = HMRAlternate(HMRConcatenate(HMRCaptureTree(@"f"), HMRConcatenate(HMRCaptureTree(@"o"), HMRConcatenate(HMRCaptureTree(@"o"), HMRCaptureTree(@"t")))), HMRConcatenate(HMRCaptureTree(@"f"), HMRConcatenate(HMRCaptureTree(@"o"), HMRConcatenate(HMRCaptureTree(@"o"), HMRCaptureTree(@"d")))));
+	
+	// put laziness in reduction, repetition, alternation, and concatenation instead
+	
 	l3_expect(derivative).to.equal(nil);
 }
