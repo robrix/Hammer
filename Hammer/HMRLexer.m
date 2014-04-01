7833eb614d3add9dc8a0928ce94a3598713d4d81
Rob Rix <rob.rix@github.com>
2014-04-01 14:13:17 -0400

    Delay the lexer sub-grammars.

--- Hammer/HMRLexer.m
+++ Hammer/HMRLexer.m
@@ -15,10 +15,12 @@
  while computing this, when we determine that we have produced a word, then perform its reduction
  */
 l3_test("null reduction of partially parsed strings") {
-	id<HMRCombinator> alternatives = HMRAlternate(HMRConcatenate(HMRLiteral(@"f"), HMRConcatenate(HMRLiteral(@"o"), HMRConcatenate(HMRLiteral(@"o"), HMRLiteral(@"t")))), HMRConcatenate(HMRLiteral(@"f"), HMRConcatenate(HMRLiteral(@"o"), HMRConcatenate(HMRLiteral(@"o"), HMRLiteral(@"d")))));
-	id<HMRCombinator> derivative = [[[alternatives derivative:@"f"] derivative:@"o"] derivative:@"o"];
+	id<HMRCombinator> alternatives = HMRAlternate(HMRDelay(HMRConcatenate(HMRDelay(HMRLiteral(@"f")), HMRDelay(HMRConcatenate(HMRDelay(HMRLiteral(@"o")), HMRDelay(HMRConcatenate(HMRDelay(HMRLiteral(@"o")), HMRDelay(HMRLiteral(@"t")))))))), HMRDelay(HMRConcatenate(HMRDelay(HMRLiteral(@"f")), HMRDelay(HMRConcatenate(HMRDelay(HMRLiteral(@"o")), HMRDelay(HMRConcatenate(HMRDelay(HMRLiteral(@"o")), HMRDelay(HMRLiteral(@"d")))))))));
+	id<HMRCombinator> derivative = [[[alternatives derivative:@"f"].compaction derivative:@"o"].compaction derivative:@"o"].compaction;
+	///Users/rob/Developer/Projects/Hammer/Hammer/HMRLexer.m:25: error: -[null_reduction_of_partially_parsed_strings derivative_should_equal_null_] : derivative ((∅ ∘ ('o' ∘ ('o' ∘ 't')) | ('o' ∘ ('o' ∘ 'd'))) | (∅ ∘ (ε↓{'o'} ∘ ('o' ∘ 't')) | (ε↓{'o'} ∘ ('o' ∘ 'd'))) | (ε↓{'f'} ∘ (∅ ∘ ('o' ∘ 't')) | (ε↓{'o'} ∘ (ε↓{'o'} ∘ 't')) | (∅ ∘ ('o' ∘ 'd')) | (ε↓{'o'} ∘ (ε↓{'o'} ∘ 'd')))) does not equal (null)
+
 	
-	id<HMRCombinator> derivative2 = HMRAlternate(HMRConcatenate(HMRCaptureTree(@"f"), HMRConcatenate(HMRCaptureTree(@"o"), HMRConcatenate(HMRCaptureTree(@"o"), HMRCaptureTree(@"t")))), HMRConcatenate(HMRCaptureTree(@"f"), HMRConcatenate(HMRCaptureTree(@"o"), HMRConcatenate(HMRCaptureTree(@"o"), HMRCaptureTree(@"d")))));
+//	id<HMRCombinator> derivative2 = HMRAlternate(HMRConcatenate(HMRCaptureTree(@"f"), HMRConcatenate(HMRCaptureTree(@"o"), HMRConcatenate(HMRCaptureTree(@"o"), HMRCaptureTree(@"t")))), HMRConcatenate(HMRCaptureTree(@"f"), HMRConcatenate(HMRCaptureTree(@"o"), HMRConcatenate(HMRCaptureTree(@"o"), HMRCaptureTree(@"d")))));
 	
 	// put laziness in reduction, repetition, alternation, and concatenation instead
 	
