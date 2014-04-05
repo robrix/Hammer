ffde6e30d67e5092ef6255cdc5284b02cc447524
Rob Rix <rob.rix@github.com>
2014-04-04 21:03:00 -0400

    Unused param style.

--- Hammer/HMRParserCombinator.m
+++ Hammer/HMRParserCombinator.m
@@ -58,7 +58,7 @@ id<HMRCombinator> HMRParseObject(id<HMRCombinator> parser, id<NSObject, NSCopyin
 			return _nullability = @([weakSelf computeNullability]);
 		}));
 		
-		_parseForest = HMRDelaySpecific([NSSet class], _parseForest = HMRLeastFixedPoint(_parseForest = [NSSet set], ^(NSSet *forest) {
+		_parseForest = HMRDelaySpecific([NSSet class], _parseForest = HMRLeastFixedPoint(_parseForest = [NSSet set], ^(NSSet *_) {
 			return _parseForest = [weakSelf reduceParseForest];
 		}));
 		
