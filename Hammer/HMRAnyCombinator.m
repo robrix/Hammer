06cdac1d9b84e0ba37c996ef68121841da8b1a2d
Rob Rix <rob.rix@github.com>
2014-04-13 16:09:26 -0400

    HMRAnyCombinator is terminal & gets its own file.

---
+++ Hammer/HMRAnyCombinator.m
@@ -0,0 +1,46 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import "HMRAnyCombinator.h"
+
+@implementation HMRAnyCombinator
+
+#pragma mark HMRCombinator
+
+-(id<HMRCombinator>)derivative:(id<NSObject,NSCopying>)object {
+	return HMRCaptureTree(object);
+}
+
+
+-(NSString *)describe {
+	return @".";
+}
+
+
+#pragma mark HMRPredicate
+
+-(bool)matchObject:(id)object {
+	return YES;
+}
+
+
+#pragma mark REDReducible
+
+-(id)red_reduce:(id)initial usingBlock:(REDReducingBlock)block {
+	return block(initial, self);
+}
+
+
+#pragma mark NSCopying
+
+-(instancetype)copyWithZone:(NSZone *)zone {
+	return self;
+}
+
+
+#pragma mark NSObject
+
+-(BOOL)isEqual:(id)object {
+	return [self matchObject:object];
+}
+
+@end
--- Hammer/HMRCase.m
+++ Hammer/HMRCase.m
@@ -1,5 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+#import "HMRAnyCombinator.h"
 #import "HMRCase.h"
 #import "HMRCombinator.h"
 #import "HMROnce.h"
@@ -62,84 +63,6 @@ l3_test(&HMRMatch) {
 }
 
 
-@interface HMRAnyCombinator : NSObject <HMRCombinator>
-@end
-
-@implementation HMRAnyCombinator
-
-#pragma mark HMRCombinator
-
--(id<HMRCombinator>)derivative:(id<NSObject,NSCopying>)object {
-	return HMRCaptureTree(object);
-}
-
-
--(bool)isNullable {
-	return NO;
-}
-
--(bool)isCyclic {
-	return NO;
-}
-
-
--(NSSet *)parseForest {
-	return [NSSet set];
-}
-
-
--(id<HMRCombinator>)compaction {
-	return self;
-}
-
-
--(NSString *)description {
-	return @"↓";
-}
-
-
-@synthesize name = _name;
-
--(instancetype)withName:(NSString *)name {
-	if (!_name) _name = [name copy];
-	return self;
-}
-
-
-#pragma mark HMRPredicate
-
--(bool)matchObject:(id)object {
-	return YES;
-}
-
--(id<HMRCase>)then:(id (^)())block {
-	return [HMRCase caseWithPredicate:self block:block];
-}
-
-
-#pragma mark REDReducible
-
--(id)red_reduce:(id)initial usingBlock:(REDReducingBlock)block {
-	return block(initial, self);
-}
-
-
-#pragma mark NSCopying
-
--(instancetype)copyWithZone:(NSZone *)zone {
-	return self;
-}
-
-
-#pragma mark NSObject
-
--(BOOL)isEqual:(id)object {
-	return [self matchObject:object];
-}
-
-@end
-
-
 @interface HMRBindCombinator : HMRAnyCombinator <HMRCombinator>
 @end
 
@@ -151,6 +74,13 @@ l3_test(&HMRMatch) {
 	NSMutableArray *bindings = (__bridge NSMutableArray *)HMRBindings;
 	[bindings addObject:object];
 	return YES;
+}
+
+
+#pragma mark NSObject
+
+-(NSString *)description {
+	return @"↓";
 }
 
 @end
