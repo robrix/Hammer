43596ea5a3a0c906460ccaac5391e21d33b91e19
Rob Rix <rob.rix@github.com>
2014-04-17 01:58:12 -0400

    Publish lots of headers.

--- Hammer/HMRNull.h
+++ Hammer/HMRNull.h
@@ -1,6 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-#import "HMRTerminal.h"
+#import <Hammer/HMRTerminal.h>
 
 @interface HMRNull : HMRTerminal
 
--- Hammer/Hammer.h
+++ Hammer/Hammer.h
@@ -1,6 +1,38 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+#pragma mark Nonterminals
+
 #import <Hammer/HMRCombinator.h>
+
+#import <Hammer/HMRNonterminal.h>
+#import <Hammer/HMRAlternation.h>
+#import <Hammer/HMRConcatenation.h>
+#import <Hammer/HMRReduction.h>
+#import <Hammer/HMRRepetition.h>
+
+
+#pragma mark Terminals
+
+#import <Hammer/HMRTerminal.h>
+
+#import <Hammer/HMREmpty.h>
+#import <Hammer/HMRNull.h>
+
+#import <Hammer/HMRPredicateCombinator.h>
+#import <Hammer/HMRContainsCombinator.h>
+#import <Hammer/HMREqualCombinator.h>
+
+
+#pragma mark Misc
+
 #import <Hammer/HMROperations.h>
 #import <Hammer/HMRParsing.h>
+
+
+#pragma mark Predicates
+
 #import <Hammer/HMRPredicate.h>
+#import <Hammer/HMRAnyCombinator.h>
+#import <Hammer/HMRBindCombinator.h>
+#import <Hammer/HMRKindOfCombinator.h>
+#import <Hammer/HMRBlockCombinator.h>
