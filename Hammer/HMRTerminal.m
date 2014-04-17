d8ec814d2cdff90f04e555cc95943c0a5a9ec28f
Rob Rix <rob.rix@github.com>
2014-04-17 01:26:02 -0400

    Defer terminal responsibilities to HMRCombinator.

--- Hammer/HMRTerminal.m
+++ Hammer/HMRTerminal.m
@@ -12,11 +12,6 @@
 }
 
 
--(bool)isNullable {
-	return NO;
-}
-
-
 -(NSSet *)parseForest {
 	return [NSSet set];
 }
@@ -38,54 +33,6 @@
 }
 
 
-@synthesize name = _name;
-
--(instancetype)withName:(NSString *)name {
-	if (!_name) _name = [name copy];
-	return self;
-}
-
-
 @dynamic hash;
-
-
-#pragma mark HMRPredicate
-
--(bool)matchObject:(id)object {
-	return ![[self derivative:object] isEqual:HMRNone()];
-}
-
--(id<HMRCase>)then:(id (^)())block {
-	return [HMRCase caseWithPredicate:self block:block];
-}
-
-
-#pragma mark REDReducible
-
--(id)red_reduce:(id)initial usingBlock:(REDReducingBlock)block {
-	return block(initial, self);
-}
-
-l3_test(@selector(red_reduce:usingBlock:)) {
-	HMRTerminal *terminal = (HMRTerminal *)HMREqual(@"x");
-	NSNumber *count = [terminal red_reduce:@0 usingBlock:^(NSNumber *into, HMRTerminal *each) {
-		return @(into.integerValue + 1);
-	}];
-	l3_expect(count).to.equal(@1);
-}
-
-
-#pragma mark NSCopying
-
--(instancetype)copyWithZone:(NSZone *)zone {
-	return self;
-}
-
-
-#pragma mark NSObject
-
--(BOOL)isEqual:(id)object {
-	return [object isKindOfClass:self.class];
-}
 
 @end
