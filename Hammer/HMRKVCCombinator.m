f3dc095048eebf157343a18c078149937b3b6ab7
Rob Rix <rob.rix@github.com>
2014-04-19 02:52:24 -0400

    KVC combinator.

---
+++ Hammer/HMRKVCCombinator.m
@@ -0,0 +1,27 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#import "HMRKeyValueCoding.h"
+#import "HMRKVCCombinator.h"
+
+@implementation HMRKVCCombinator
+
++(instancetype)keyPath:(NSString *)keyPath combinator:(HMRCombinator *)combinator {
+	return [[self alloc] initWithKeyPath:keyPath combinator:combinator];
+}
+
+-(instancetype)initWithKeyPath:(NSString *)keyPath combinator:(HMRCombinator *)combinator {
+	if ((self = [super init])) {
+		_keyPath = [keyPath copy];
+		_combinator = [combinator copy];
+	}
+	return self;
+}
+
+
+#pragma mark HMRCombinator
+
+-(HMRCombinator *)derivative:(id<NSObject,NSCopying,HMRKeyValueCoding>)object {
+	return [self.combinator derivative:[object valueForKeyPath:self.keyPath]];
+}
+
+@end
