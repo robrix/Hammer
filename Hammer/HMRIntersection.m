50db9d776c81ddcedd07d74620c8aa360a90fb9d
Rob Rix <rob.rix@github.com>
2014-04-20 10:05:33 -0400

    Already compacted intersections compact to self.

--- Hammer/HMRIntersection.m
+++ Hammer/HMRIntersection.m
@@ -38,6 +38,8 @@ l3_test(@selector(derivative:)) {
 	HMRCombinator *right = self.right.compaction;
 	if ([left isEqual:[HMRCombinator empty]] || [right isEqual:[HMRCombinator empty]])
 		compacted = [HMRCombinator empty];
+	else if (left == self.left && right == self.right)
+		compacted = self;
 	else
 		compacted = [left and:right];
 	return compacted;
