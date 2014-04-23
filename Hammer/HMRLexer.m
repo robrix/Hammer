8f7124de3fe9d7544fe0df0a7a97b3cae23fbaa1
Rob Rix <rob.rix@github.com>
2014-04-22 22:22:55 -0400

    Disambiguate the grammar.

--- Hammer/HMRLexer.m
+++ Hammer/HMRLexer.m
@@ -7,7 +7,47 @@
 #import "HMRReduction.h"
 #import <Reducers/REDReducer.h>
 
+id<REDReducible> HMRLex(id<REDReducible> input, id<REDReducible> patterns) {
+	__block HMRCombinator *into = [[[[HMRCombinator alternate:patterns] or:[HMRCombinator null]] concat:HMRDelay(into)] withName:@"S"];
+	
+	return [[input red_reduce:into usingBlock:^(HMRCombinator *into, id each) {
+		return [into derivative:each];
+	}] parseForest];
+}
+
+@interface HMRToken : NSObject
+@end
+
 l3_test("lexer grammar") {
+	NSMutableCharacterSet *first = [NSMutableCharacterSet letterCharacterSet];
+	[first addCharactersInString:@"_-"];
+	NSMutableCharacterSet *rest = [NSMutableCharacterSet alphanumericCharacterSet];
+	[rest addCharactersInString:@"_-"];
+	
+	HMRCombinator *symbol = [[[[[[HMRCombinator containedIn:first] withName:@"first"] concat:[[[HMRCombinator containedIn:rest] withName:@"rest"] repeat]] map:^(id<REDReducible> each) {
+		return [@"" red_append:each];
+	}] withFunctionDescription:@"stringify"] withName:@"symbol"];
+	
+	HMRCombinator *whitespaceSet = [HMRCombinator containedIn:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	HMRCombinator *whitespace = [[[whitespaceSet concat:[whitespaceSet repeat]] ignore] withName:@"whitespace"];
+	
+//	HMRCombinator *
+	
+//	id lexed = HMRLex(@"colourless green ideas sleep furiously", @[
+//		symbol,
+//		whitespace,
+//	]);
+	
+	HMRCombinator *into = [symbol concat:[[whitespace concat: symbol] repeat]];
+	id input = @"colourless green ideas sleep furiously";
+	id lexed = [[input red_reduce:into usingBlock:^(HMRCombinator *into, id each) {
+		return [into derivative:each];
+	}] parseForest];
+	
+	l3_expect(lexed).to.equal(@[@"colourless", @"green", @"ideas", @"sleep", @"furiously"]);
+}
+
+l3_test("lexer incrementality") {
 	NSMutableArray *reductions = [NSMutableArray new];
 	HMRCombinator *wordSet = [HMRCombinator containedIn:[NSCharacterSet alphanumericCharacterSet]];
 	HMRCombinator *word = [[[[wordSet concat:[wordSet repeat]] map:^id<NSObject,NSCopying>(id<NSObject,NSCopying> x) {
@@ -33,3 +73,7 @@ l3_test("lexer grammar") {
 	l3_expect(parseForest).to.equal([NSSet setWithObject:HMRList(@"one", @"fish", @"two", @"fish", @"red", @"fish", @"blue", @"fish", nil)]);
 	l3_expect(reductions).to.equal(@[ @"one", @"fish", @"two", @"fish", @"red", @"fish", @"blue", @"fish" ]);
 }
+
+
+@implementation HMRToken
+@end
